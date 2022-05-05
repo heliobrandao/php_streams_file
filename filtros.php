8f94bb1845e307aaa6cf96d430d4fa500874344b
heliobrandao <heliob79@gmail.com>
2022-05-04 23:28:48 -0300

    lesson-03

---
+++ filtros.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'MeuFiltro.php';
+
+$arquivoCursos = fopen('lista-cursos.txt', 'r');
+
+stream_filter_register('alura.partes', MeuFiltro::class);
+
+stream_filter_append($arquivoCursos, 'alura.partes');
+// stream_filter_append($arquivoCursos, 'string.toupper');
+
+
+echo fread($arquivoCursos, filesize('lista-cursos.txt'));
